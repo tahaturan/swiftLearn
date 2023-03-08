3f7fb4d02baee909e3fcd5434f283d04f7c9cb8f
tahaturan <tahaaturann16@gmail.com>
2023-03-08 05:34:26 +0300

    ddegiskenler

---
+++ variables.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var myNumber = 5 * 4
+
+print(myNumber)
+
+var numberOne = 2
+var numberTwo = 15
+
+var sum = numberOne + numberTwo
+print(sum)
