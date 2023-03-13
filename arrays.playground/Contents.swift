5a87a712a5c5849773f9caf7e4bc6a8358404dc8
tahaturan <tahaaturann16@gmail.com>
2023-03-13 15:55:10 +0300

    Dictionary

--- arrays.playground/Contents.swift
+++ arrays.playground/Contents.swift
@@ -26,3 +26,20 @@ var myStringSet : Set = ["b" ,"c" , "f" , "s"]
 var myInternetArray = [1,2,3,4,5,6,1,2,3,4,1,3]
 
 var myInternetSet = Set(myInternetArray)
+
+//Dictionary
+
+var myDictionary = ["name" :"taha" , "number": "12314124" , "age":"25"]
+
+myDictionary["name"]
+
+myDictionary["name"] = "Taha Turan"
+myDictionary["name"]
+
+myDictionary["addres"] = "Aydin"
+
+print(myDictionary)
+
+var myAnyDictonary = ["Run":150 , "swim":200]
+
+myAnyDictonary["Run"]
