5bd67316795dcdce20ebf7b0147a9c5cfb4093dc
tahaturan <tahaaturann16@gmail.com>
2023-03-14 05:23:03 +0300

    For Loop

--- loops.playground/Contents.swift
+++ loops.playground/Contents.swift
@@ -7,5 +7,29 @@ while myNumber < 10 {
     myNumber += 1
 }
         
+//For Loop
+
+var myArray = ["banana" , "Apple" , "Orange"]
+
+for item in myArray {
+    print(item)
+}
+print("*****************************")
+
+var myNumberArray = [10,20,30,40,50,60,70]
+
+for item in myNumberArray{
+    print(item / 5)
+}
 
 
+for mynewint in 1 ... 5 {
+    print(mynewint)
+}
+
+var myNewList = [1]
+
+for int in 2 ... 10 {
+    myNewList.append(int)
+}
+print(myNewList)
