706a13a225f370e2289795c48c99101ec005d86a
tahaturan <tahaaturann16@gmail.com>
2023-03-23 00:05:42 +0300

    tur donusumleri

--- exercise .playground/Contents.swift
+++ exercise .playground/Contents.swift
@@ -22,3 +22,32 @@ let pi = 3.14
 print(pi)
 
 // sabitler hafiyazi rahatlatir ve ddegistireemezler
+
+//Tur Donusumleri
+
+//sayisaldan sayisala
+
+var i:Int = 42
+var d:Double = 32.6
+var f:Float = 43.2
+
+var sonuc1 :Int = Int(d)
+var sonuc2 : Double = Double(i)
+var sonuc3 :Int = Int(f)
+//sayisaldan metine
+var str1 :String = String(i)
+
+// metinden sayisala
+
+var worldOne :String = "234"
+
+
+if let sayi1 = Int(worldOne){
+    print(sayi1)
+}
+
+var yazi1 :String = "124.53"
+
+if let sayi1 = Double(yazi1){
+    print(sayi1)
+}
