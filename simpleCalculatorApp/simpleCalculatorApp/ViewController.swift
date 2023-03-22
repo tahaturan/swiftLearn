bf170b1ecda9c46beaa128e383fd3a55b4b9ee4c
tahaturan <tahaaturann16@gmail.com>
2023-03-22 23:23:51 +0300

    calculator and exercise

--- simpleCalculatorApp/simpleCalculatorApp/ViewController.swift
+++ simpleCalculatorApp/simpleCalculatorApp/ViewController.swift
@@ -8,12 +8,63 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var firstText: UITextField!
+    @IBOutlet weak var secontText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var result = 0.0
+    
+ 
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    
+    @IBAction func SumClicked(_ sender: Any) {
+        if let firstNumber = Double(firstText.text!){
+            if  let secontNumber = Double(secontText.text!){
+                result = firstNumber + secontNumber
+                resultLabel.text = String(result)
+            }
+        }
+        
+       
+     
+    }
+    
+    @IBAction func minusClicked(_ sender: Any) {
+        if let firstNumber = Double(firstText.text!){
+            if  let secontNumber = Double(secontText.text!){
+                result = firstNumber - secontNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
 
+    @IBAction func multiplyClicked(_ sender: Any) {
+        if let firstNumber = Double(firstText.text!){
+            if  let secontNumber = Double(secontText.text!){
+                result = firstNumber * secontNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
+    @IBAction func divideClicked(_ sender: Any) {
+        if let firstNumber = Double(firstText.text!){
+            if  let secontNumber = Double(secontText.text!){
+                result = firstNumber / secontNumber
+                resultLabel.text = String(result)
+            }
+        }
+    }
+    
 }
 
